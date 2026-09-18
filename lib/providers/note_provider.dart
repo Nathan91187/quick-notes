39dea4747fad96311bc223912519c1e58396017f
Nathan91187 <natanasnake91@gmail.com>
2026-09-18 21:28:49 +0300

    added fetch and send methods in note provider

--- lib/providers/note_provider.dart
+++ lib/providers/note_provider.dart
@@ -1,23 +1,49 @@
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:quick_notes/models/note.dart';
+import 'package:quick_notes/services/note_service.dart';
 
 class NoteProvider extends ChangeNotifier{
-  final List<NoteModel> noteList = [
-  NoteModel(
-        createdAt: DateTime.now(),
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        noteID: "1",
-        noteTitle: "Note 1"),
-    NoteModel(
-        createdAt: DateTime.now(),
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        noteID: "2",
-        noteTitle: "Note 2"),
-    NoteModel(
-        createdAt: DateTime.now(),
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        noteID: "3",
-        noteTitle: "Note 3")
+  bool isLoading = false;
+  String? errorMessage;
+  final NoteService noteService = NoteService();
+   List<NoteModel> noteList = [
   ];
+  void deleteNote(String noteId){
+    noteList.removeWhere((note)=> note.noteID == noteId);
+    notifyListeners();
+  }
+  Future<void> addNote(NoteModel note)async{
+    isLoading = true;
+    errorMessage = null;
+    notifyListeners();
+    try{
+      NoteModel currNote = await noteService.createNote(note);
+      noteList.add(currNote);
+    }catch(e){
+      errorMessage = "Failed to add note";
+    }
+    finally{
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+  Future<void> getNotes() async{
+    isLoading = true;
+    errorMessage = null;
+    notifyListeners();
+    try {
+      final notes = await noteService.getNotes();
+      noteList = notes;
+    } catch (e) {
+      errorMessage = "Failed to load notes";
+    }
+    finally{
+      isLoading = false;
+      notifyListeners();
+    }
+
+    isLoading = false;
+    notifyListeners();
+  }
 }
