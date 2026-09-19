9a2cb7be78c578ec5f0b2af4a98ad5c8885d8882
Nathan91187 <natanasnake91@gmail.com>
2026-09-19 19:54:46 +0300

    included patch and delete methods

--- lib/services/note_service.dart
+++ lib/services/note_service.dart
@@ -3,16 +3,24 @@ import 'dart:convert';
 import 'package:quick_notes/models/note.dart';
 import 'package:http/http.dart';
 class NoteService {
+  Future<void> deleteNote(String noteID) async{
+    final response = await delete(
+        Uri.parse("https://dummyjson.com/posts/$noteID"));
+    if(response.statusCode < 200 || response.statusCode >= 300){
+      throw Exception("Failed to delete note");
+    }
+  }
   Future<List<NoteModel>> getNotes() async{
     final response = await get(
       Uri.parse("https://dummyjson.com/posts")
     );
     if(response.statusCode == 200){
       final data = jsonDecode(response.body);
-     return data['posts'].map(NoteModel.fromJson).toList();
+      final List<dynamic> posts = data['posts'];
+     return posts.map((post)=> NoteModel.fromJson(post)).toList();
     }
     else{
-     throw Exception("failed to load members");
+     throw Exception("failed to load notes");
     }
   }
   Future<NoteModel> createNote(NoteModel note) async{
@@ -20,7 +28,8 @@ class NoteService {
           Uri.parse("https://dummyjson.com/posts/add"),
           body: jsonEncode({
             ...note.toJson(),
-                'userId': 1,}
+                'userId': 1,
+          }
           ),
           headers: {
             'content-type' : 'application/json',
@@ -32,5 +41,19 @@ class NoteService {
       final json = jsonDecode(response.body);
       return NoteModel.fromJson(json);
 
+  }
+  Future<NoteModel> editNote(String noteID, Map<String,dynamic> updates) async{
+    final response = await patch(
+      Uri.parse("https://dummyjson.com/posts/$noteID"),
+      body: jsonEncode(updates),
+      headers: {
+        'content-type' : 'application/json'
+      }
+    );
+    if(response.statusCode < 200 || response.statusCode >= 300){
+      throw Exception("Failed to update note");
+    }
+    final json = jsonDecode(response.body);
+    return NoteModel.fromJson(json);
   }
 }
